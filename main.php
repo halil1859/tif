9190f1d3cafc4e996dfa21361ff8c6b0a4a50db1
halil1859 <halil1859@hotmail.com>
2018-09-13 00:07:15 +0300

    ilk main sayfası
    sadece bölümlere ayırma yapıldı. sütun dizaynı verildi. bundan sonra ürünleri otomatik getirme yapılır. ürünleri güncelleme için modal, ürün bilgilerini güncelleme, giriş tife gönder çıkış tif e gönder yapmak gerekir.

---
+++ main.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="tr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <style media="screen">
+      .photoBox{
+        background-color:pink;
+      }
+      .urunAdiBox{
+        background-color:green;
+      }
+      .thumbnail a>img, .thumbnail>img{
+        background-color:grey;
+        height: 300px;
+        width: 100%;
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+      <nav>
+        <h1>navigasyon</h1>
+      </nav>
+    </section>
+    <section>
+      <h1>main</h1>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 col-md-4 col-sm-12" style="background-color:yellow;">
+
+            <div class="thumbnail">
+              <img src="..." alt="...">
+              <p>Ürün fotoğrafı eklenmemiş</p>
+              <div class="caption">
+                <h3>Ürün Adı</h3>
+                <p>Ürün hakkında bilgiler</p>
+                <p><a href="#" class="btn btn-primary" role="button">Giriş yap</a> <a href="#" class="btn btn-default" role="button">Çıkış yap</a> </p>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-lg-3 col-md-4 col-sm-12" style="background-color:pink;">
+            <div class="thumbnail">
+              <img src="..." alt="...">
+              <p>Ürün fotoğrafı eklenmemiş</p>
+              <div class="caption">
+                <h3>Ürün Adı</h3>
+                <p>Ürün hakkında bilgiler</p>
+                <p><a href="#" class="btn btn-primary" role="button">Giriş yap</a> <a href="#" class="btn btn-default" role="button">Çıkış yap</a> </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 col-sm-12" style="background-color:yellow;">
+
+            <div class="thumbnail">
+              <img src="..." alt="...">
+              <p>Ürün fotoğrafı eklenmemiş</p>
+              <div class="caption">
+                <h3>Ürün Adı</h3>
+                <p>Ürün hakkında bilgiler</p>
+                <p><a href="#" class="btn btn-primary" role="button">Giriş yap</a> <a href="#" class="btn btn-default" role="button">Çıkış yap</a> </p>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-lg-3 col-md-4 col-sm-12" style="background-color:pink;">
+            <div class="thumbnail">
+              <img src="..." alt="...">
+              <p>Ürün fotoğrafı eklenmemiş</p>
+              <div class="caption">
+                <h3>Ürün Adı</h3>
+                <p>Ürün hakkında bilgiler</p>
+                <p><a href="#" class="btn btn-primary" role="button">Giriş yap</a> <a href="#" class="btn btn-default" role="button">Çıkış yap</a> </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <footer>
+        <h1>footer</h1>
+      </footer>
+    </section>
+
+  </body>
+</html>
